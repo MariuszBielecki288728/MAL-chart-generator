3aad623cc3b8b910e70243f3757017d882aeb493
Mariusz <288728@uwr.edu.pl>
2018-01-29 05:37:49 +0100

    new template and main in dev

--- MAL_chart_generator/main.py
+++ MAL_chart_generator/main.py
@@ -1,6 +1,6 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
-
+import plots_generator
 
 def load_obj(name):
     with open(name + '.json', 'r') as f:
@@ -12,7 +12,7 @@ env = Environment(
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-template = env.get_template('result_template.html')
+template = env.get_template('result_template2.html')
 manga_dict = load_obj('test_save')
 link = 'https://myanimelist.net/manga/'
 masterpieces = [{'image': dict_['image'],
@@ -20,8 +20,20 @@ masterpieces = [{'image': dict_['image'],
                  'name': dict_['name']}
                 for id_, dict_ in manga_dict.items()
                 if dict_['user_score'] == '10']
-
-rendered = template.render(masterpiece_list=masterpieces)
+# plots_generator.draw_genres_pie(manga_dict, '')
+masterpieces_dif_img = 'mastepieces_dif.png'
+plots_generator.draw_avg_vs_u10_stem(manga_dict, masterpieces_dif_img)
+big_dif_img = 'big_dif.png'
+plots_generator.draw_biggest_dif_stem(manga_dict, big_dif_img)
+small_dif_img = 'small_dif.png'
+plots_generator.draw_biggest_dif_stem(manga_dict, small_dif_img, rev=True)
+rendered = template.render(user_name='MariuszB',
+                           titles_num=20,
+                           masterpiece_list=masterpieces,
+                           masterpieces_dif_img=masterpieces_dif_img,
+                           big_dif_img=big_dif_img,
+                           small_dif_img=small_dif_img
+                           )
 
 with open('result.html', 'w') as f:
     f.write(rendered)
